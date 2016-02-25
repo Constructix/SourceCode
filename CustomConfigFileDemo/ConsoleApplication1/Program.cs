e4d501537fea7cc7d226e577f1a13c263cf507d3
Richard Jones <r_jones@constructix.com.au>
2016-02-25 14:14:01 +1000

    checking in to add to github

---
+++ CustomConfigFileDemo/ConsoleApplication1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CustomConfigFileDemo;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (FileElement file in FileRetriever.GetFiles())
+            {
+                Console.WriteLine(file.SourcePath);
+                Console.WriteLine(file.DestinationPath);
+                //DO WORK 
+            }
+
+        }
+    }
+}
