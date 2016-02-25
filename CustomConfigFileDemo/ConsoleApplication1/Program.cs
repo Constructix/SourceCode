6e974af257bbec99f03e7bf3bf648547fb3dd9ea
Richard Jones <r_jones@constructix.com.au>
2016-02-25 14:30:44 +1000

    Updated main console.

--- CustomConfigFileDemo/ConsoleApplication1/Program.cs
+++ CustomConfigFileDemo/ConsoleApplication1/Program.cs
@@ -9,13 +9,15 @@ namespace ConsoleApplication1
 {
     class Program
     {
+
+        // get the settings from the config file.
         static void Main(string[] args)
         {
             foreach (FileElement file in FileRetriever.GetFiles())
             {
                 Console.WriteLine(file.SourcePath);
                 Console.WriteLine(file.DestinationPath);
-                //DO WORK 
+                
             }
 
         }
