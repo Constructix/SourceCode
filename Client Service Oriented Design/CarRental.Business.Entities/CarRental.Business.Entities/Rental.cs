fe325eac470ffa46f45a5d249900465a7390f23f
Richard Jones <r_jones@constructix.com.au>
2016-02-29 19:57:34 +1000

    added interface to Rental Object.

--- Client Service Oriented Design/CarRental.Business.Entities/CarRental.Business.Entities/Rental.cs
+++ Client Service Oriented Design/CarRental.Business.Entities/CarRental.Business.Entities/Rental.cs
@@ -1,7 +1,10 @@
-﻿namespace CarRental.Business.Entities
+﻿using Core.Common.Contracts;
+using Core.Common.Core;
+
+namespace CarRental.Business.Entities
 {
-    public class Rental
+    public class Rental : EntityBase, IIdentifiableEntity
     {
-         
+        public int EntityId { get; set; }
     }
 }
