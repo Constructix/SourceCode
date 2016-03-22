a534b7b1671cd2f4bea1d8f2bf28d982d7027079
Richard Jones <rjjones79@hotmail.com>
2016-03-22 22:11:17 +1000

    Working version of communicating to WCF

---
+++ WCF-End-To-End/GeoLib.Client/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GeoLib.Contracts;
+using GeoLib.Proxies;
+
+namespace GeoLib.Client
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(textBox.Text))
+            {
+                GeoClient proxy = new GeoClient();
+                ZipCodeData data = proxy.GetZipInfo(textBox.Text);
+                if (data != null)
+                {
+                    StringBuilder result = new StringBuilder();
+                    result.AppendLine(data.City);
+                    result.AppendLine(data.State);
+
+                    lblResultGetInfo.Content = result.ToString();
+                }
+                else
+                {
+                    lblResultGetInfo.Content = "No Data found.";
+                }
+                proxy.Close();
+            }
+        }
+    }
+}
--- WCF-End-To-End/GeoLib.ConsoleHost/Program.cs
+++ WCF-End-To-End/GeoLib.ConsoleHost/Program.cs
@@ -15,12 +15,11 @@ namespace GeoLib.ConsoleHost
         static void Main(string[] args)
         {
             ServiceHost hostGeoManager = new ServiceHost(typeof(GeoManager));
-
-            string address = "net.tcp://localhost:8009/GeoService";
-            Binding binding = new NetTcpBinding();
-            Type contract = typeof (IGeoService);
-            hostGeoManager.AddServiceEndpoint(contract, binding, address);
-
+           
+            //string address = "net.tcp://localhost:8009/GeoService";
+            //Binding binding = new NetTcpBinding();
+            //Type contract = typeof (IGeoService);
+            //hostGeoManager.AddServiceEndpoint(contract, binding, address);
 
             hostGeoManager.Open();
             Console.WriteLine("Services [GeoManager] started. Press [Enter] to exit.");
