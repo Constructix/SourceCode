8210cc08048ceacbd087ceab6d9ac98798297a32
Richard Jones <rjjones79@hotmail.com>
2016-03-28 20:43:57 +1000

    Added Console class for some unknown reason the console code was not include in the initial checkin to GitHub

---
+++ HackerRank/TimeConversion/TimeConversion/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string time = Console.ReadLine();
+
+            ITimeConverter converter = new TimeConverter();
+            try
+            {
+                Console.WriteLine(converter.Convert(time));
+            }
+            catch (Exception ex)
+            {
+                
+                Console.WriteLine( ex.Message);
+            }
+        }
+    }
+
+    public class TimeConverter : ITimeConverter
+    {
+        public string Convert(string inputTime)
+        {
+            DateTime inputTimeAsDateTime;
+            if (!DateTime.TryParse(inputTime, out inputTimeAsDateTime))
+            {
+                throw new Exception("Input time is not Valid.Please enter in format: hh:mm:ssAM/PM");
+            }
+            else
+            {
+                if (inputTimeAsDateTime.Hour < 0 || inputTimeAsDateTime.Hour > 23)
+                    throw new Exception("Input time is not Valid.Please enter in format: hh:mm:ssAM/PM");
+
+
+                int hour = inputTimeAsDateTime.Hour;
+                int minute = inputTimeAsDateTime.Minute;
+                int seconds = inputTimeAsDateTime.Second;
+                return $"{hour:00}:{minute:00}:{seconds:00}";
+            }
+        }
+    }
+
+    public interface ITimeConverter
+    {
+        string Convert(string inputTime);
+    }
+}
