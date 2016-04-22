1dab3ce37606c331f10523039d17bb7b84b4ed8e
Richard Jones <rjjones79@hotmail.com>
2016-04-22 18:45:38 +1000

    Algorithms folder created and initial checkin of quickSort

---
+++ Algorithms/QuickSort/Algorithims/QuickSort/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new[] {13, 19, 9, 5, 12, 8, 7, 4, 21, 2, 6, 11};
+            
+            QuickSort(array, 0, array.Length-1);
+
+
+        }
+
+        static void QuickSort(int[] array, int p, int r)
+        {
+            if (p < r)
+            {
+                int q = Partition(array, p, r);
+                QuickSort(array, p, q - 1);
+                QuickSort(array, q + 1, r);
+            }
+        }
+
+        private static int Partition(int[] array, int p, int r)
+        {
+            int x = array[r];
+            int i = p - 1;
+
+            for (int j = p; j <= r - 1; j++)
+            {
+                if (array[j] <= x)
+                {
+                    i++;
+                    Swap(array, j, i);
+
+                }
+            }
+            Swap(array, i + 1, r);
+            return i + 1;
+        }
+
+        private static void Swap(int[] array, int x, int y)
+        {
+            int temp = array[y];
+            array[y] = array[x];
+            array[x] = temp;
+        }
+    }
+}
--- WCF/WCFCustomBehavors/WCFCustomBehavors/MerchantManager.cs
+++ WCF/WCFCustomBehavors/WCFCustomBehavors/MerchantManager.cs
@@ -7,7 +7,7 @@ using WCFCustomBehaviors.Contracts;
 
 namespace WCFCustomBehaviors.Service
 {
-    public class MerchantManager: IMerchantService, IServiceBehavior
+    public class MerchantManager: IMerchantService
     {
         public string GetClientId(string email)
         {
@@ -33,32 +33,5 @@ namespace WCFCustomBehaviors.Service
             
             return new SubmitResponse {Status = "OK"};
         }
-
-        #region IServiceBehavior
-        public void Validate(ServiceDescription serviceDescription, ServiceHostBase serviceHostBase)
-        {
-            
-        }
-
-        public void AddBindingParameters(ServiceDescription serviceDescription, ServiceHostBase serviceHostBase, Collection<ServiceEndpoint> endpoints,
-            BindingParameterCollection bindingParameters)
-        {
-            
-        }
-
-        public void ApplyDispatchBehavior(ServiceDescription serviceDescription, ServiceHostBase serviceHostBase)
-        {
-            foreach (ServiceEndpoint endpoint in  serviceDescription.Endpoints)
-            {
-                foreach (OperationDescription operation in endpoint.Contract.Operations)
-                {
-                    OperationReportOperationBehavior operationReportOperationBehavior = new OperationReportOperationBehavior();
-                    operation.OperationBehaviors.Add(operationReportOperationBehavior);
-                }
-            }
-          
-        }
-        #endregion
-
     }
 }
