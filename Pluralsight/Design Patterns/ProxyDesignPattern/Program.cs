44f70898a133b3e1569f32554d3ca2f6f5679bdf
Richard Jones <r_jones@constructix.com.au>
2016-08-31 19:55:22 +1000

    Added Proxy pattern

---
+++ Pluralsight/Design Patterns/ProxyDesignPattern/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProxyDesignPattern
+{
+    class Program
+    {
+        // provide a surrogate or placeholder for another object to control access to it.
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
