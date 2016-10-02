b2e4fc06a12daf95189975acde854596d99537fd
Richard Jones <r_jones@constructix.com.au>
2016-10-02 18:27:07 +1100

    Added FuncOfT Methods

--- Pluralsight/EventsDelegatesAndLambdas.Tests/LambdasAndDelegates/Program.cs
+++ Pluralsight/EventsDelegatesAndLambdas.Tests/LambdasAndDelegates/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,7 +17,28 @@ namespace LambdasAndDelegates
             BizRulesDelegate addDel = (x, y) => x + y;
             BizRulesDelegate addMultiply = (x, y) => x*y;
 
-            data.Process(2,3, addDel);
+            //data.Process(2,3, addDel);
+            //DemostrationOfActionOfT(data);
+
+            DemostrationOfFuncOfT(data);
+        }
+
+        private static void DemostrationOfFuncOfT(ProcessData data)
+        {
+            Func<int, int, int> funcAddDel = (x, y) => x + y;
+            Func<int, int, int> funcMultiplyDel = (x, y) => x*y;
+
+            data.ProcessFunc(2, 3, funcAddDel);
+            data.ProcessFunc(2, 3, funcMultiplyDel);
+        }
+
+        private static void DemostrationOfActionOfT(ProcessData data)
+        {
+            Action<int, int> myAction = (x, y) => Console.WriteLine(x + y);
+            Action<int, int> myMultiplyAction = (x, y) => Console.WriteLine(x*y);
+
+            data.ProcessAction(2, 3, myAction);
+            data.ProcessAction(2, 3, myMultiplyAction);
         }
     }
 
@@ -33,6 +55,12 @@ namespace LambdasAndDelegates
             action(x, y);
             Console.WriteLine("Action has been processed.");
             
+        }
+
+        public void ProcessFunc(int x, int y, Func<int, int, int> func)
+        {
+            var result =  func(x, y);
+            Console.WriteLine(result);
         }
     }
 }
