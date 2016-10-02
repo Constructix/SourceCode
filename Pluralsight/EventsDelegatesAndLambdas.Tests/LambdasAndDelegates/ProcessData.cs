5c74f9fd7042ef6c5d2c96840dfeda757f6de6f7
Richard Jones <r_jones@constructix.com.au>
2016-10-02 18:50:34 +1100

    Checked into GitHub

---
+++ Pluralsight/EventsDelegatesAndLambdas.Tests/LambdasAndDelegates/ProcessData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace LambdasAndDelegates
+{
+    public class ProcessData
+    {
+        public void Process(int x, int y, BizRulesDelegate del)
+        {
+            var result = del(x, y);
+            Console.WriteLine(result);
+        }
+
+        public void ProcessAction(int x, int y, Action<int, int> action)
+        {
+            action(x, y);
+            Console.WriteLine("Action has been processed.");
+            
+        }
+
+        public void ProcessFunc(int x, int y, Func<int, int, int> func)
+        {
+            var result =  func(x, y);
+            Console.WriteLine(result);
+        }
+    }
+}
--- Pluralsight/EventsDelegatesAndLambdas.Tests/LambdasAndDelegates/Program.cs
+++ Pluralsight/EventsDelegatesAndLambdas.Tests/LambdasAndDelegates/Program.cs
@@ -41,26 +41,4 @@ namespace LambdasAndDelegates
             data.ProcessAction(2, 3, myMultiplyAction);
         }
     }
-
-    public class ProcessData
-    {
-        public void Process(int x, int y, BizRulesDelegate del)
-        {
-            var result = del(x, y);
-            Console.WriteLine(result);
-        }
-
-        public void ProcessAction(int x, int y, Action<int, int> action)
-        {
-            action(x, y);
-            Console.WriteLine("Action has been processed.");
-            
-        }
-
-        public void ProcessFunc(int x, int y, Func<int, int, int> func)
-        {
-            var result =  func(x, y);
-            Console.WriteLine(result);
-        }
-    }
 }
