1291ab3e305a224aed1961a6651d012c7da5cb83
Richard Jones <r_jones@constructix.com.au>
2016-10-02 18:13:58 +1100

    Added Lambdas and delegates console to solution.

---
+++ Pluralsight/EventsDelegatesAndLambdas.Tests/LambdasAndDelegates/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdasAndDelegates
+{
+
+    public delegate int BizRulesDelegate(int x, int y);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = new ProcessData();
+            BizRulesDelegate addDel = (x, y) => x + y;
+            BizRulesDelegate addMultiply = (x, y) => x*y;
+
+            data.Process(2,3, addDel);
+        }
+    }
+
+    public class ProcessData
+    {
+        public void Process(int x, int y, BizRulesDelegate del)
+        {
+            var result = del(x, y);
+            Console.WriteLine(result);
+        }
+
+        public void ProcessAction(int x, int y, Action<int, int> action)
+        {
+            action(x, y);
+            Console.WriteLine("Action has been processed.");
+            
+        }
+    }
+}
