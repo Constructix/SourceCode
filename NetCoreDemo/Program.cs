e6338bf36b570bc1f0df22c0788a8ead1f7823a1
Richard Jones <r_jones@constructix.com.au>
2016-10-03 18:52:02 +1100

    First .NET Core application to be published.

---
+++ NetCoreDemo/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Testing Person instance");
+
+            var p = new Person { FirstName = "Richard", LastName = "Jones"};
+
+            Console.WriteLine(p);
+            Console.WriteLine(p.Created);
+
+            Console.WriteLine(System.Globalization.CultureInfo.CurrentCulture.EnglishName);
+
+        }
+    }
+
+
+    public class Person
+    {
+        public string FirstName {get;set;}
+        public string LastName {get;set;}
+
+        public DateTime Created {get;set;}
+
+
+        public Person ()
+        {
+          Created = DateTime.Now;
+        }
+
+        public override string ToString()
+        {
+            return string.Format($"{FirstName} {LastName}");
+        }
+    }
+}
