c7b60017c2e3823cb5c9730fe7dc211e6914949d
Richard Jones <r_jones@constructix.com.au>
2016-11-07 18:11:54 +1000

    Added Base Service Test

---
+++ Mocking/MockingDemo/BaseServiceTest/Class1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseServiceTest
+{
+    public class Order
+    {
+        public DateTime Created { get; set; }
+
+        public int Id { get; set; }
+    }
+
+
+    public class Customer
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+    }
+
+
+    public class OrderInputData
+    {
+        public DateTime Created { get; set; }
+    }
+
+    public class CustomerInputData
+    {
+        public string FirstName { get; set; }
+    }
+
+
+
+   
+    public interface IRepository<TEntity>
+    {
+        void Add(TEntity entity);
+        void Remove(TEntity entity);
+        IQueryable<TEntity> Get(Func<TEntity, bool> query);
+    }
+
+    public abstract class BaseRepository<TEntity> : IRepository<TEntity>
+    {
+        protected List<TEntity> _entities;
+
+        public BaseRepository()
+        {
+            _entities = new List<TEntity>();
+        }
+        public virtual void Add(TEntity entity)
+        {
+            _entities.Add(entity);
+            Console.WriteLine($"Added {entity.GetType().FullName}");
+        }
+
+        public void Remove(TEntity entity)
+        {
+            _entities.Remove(entity);
+        }
+
+        public IQueryable<TEntity> Get(Func<TEntity, bool> query)
+        {
+            return _entities.Where(query).AsQueryable();
+        }
+
+        public IEnumerable<TEntity> Get()
+        {
+            return _entities.ToList();
+        }
+    }
+
+    public class CustomerRepository : BaseRepository<Customer>
+    {
+       
+    }
+
+    public class OrderRepository : BaseRepository<Order>
+    {
+        
+    }
+
+    public interface IFactory<TObject, TInputData>
+    {
+        TObject Create(TInputData inputData);
+    }
+
+    public abstract class BaseFactory<TObject, TInputData> : IFactory<TObject, TInputData>
+    {
+        public abstract TObject Create(TInputData inputData);
+    }
+
+    public class CustomerFactory : BaseFactory<Customer, CustomerInputData>
+    {
+        public override Customer Create(CustomerInputData customerInputData)
+        {
+            return new Customer { FirstName = customerInputData.FirstName };
+        }
+    }
+
+
+
+    public class OrderFactory : IFactory<Order, OrderInputData>
+    {
+        public Order Create(OrderInputData inputData)
+        {
+            return new Order {Created = inputData.Created};
+        }
+    }
+
+
+}
---
+++ Mocking/MockingDemo/BaseServiceTest/Test.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using NUnit.Framework;
+
+namespace BaseServiceTest
+{
+    [TestFixture]
+    public class CustomerRepositoryTests
+    {
+        [Test]
+        public void CustomerRepository()
+        {
+            Customer newCustomer = new Customer() {FirstName = "Richard"};
+
+            var custRepo = new CustomerRepository();
+
+            custRepo.Add(newCustomer);
+            custRepo.Add(new Customer { FirstName = "Teresa"});
+
+
+            var customers = custRepo.Get(x=>!string.IsNullOrEmpty(x.FirstName)).ToList();
+
+            foreach (var customer in customers)
+            {
+                Console.WriteLine(customer.FirstName);
+            }
+        }
+
+
+        [Test]
+        public void OrderRepository_Instance_Created()
+        {
+            var orderRepo = new OrderRepository();
+
+            orderRepo.Add(new Order {Created =  DateTime.Today});
+            orderRepo.Add(new Order { Created = DateTime.Today.AddDays(-3) });
+            orderRepo.Add(new Order { Created = DateTime.Today.AddDays(-6) });
+            orderRepo.Add(new Order { Created = DateTime.Today.AddDays(-16) });
+
+            var orders = orderRepo.Get();
+
+            foreach (Order currentOrder in orders)
+            {
+                Console.WriteLine(currentOrder.Created.ToString());
+            }
+        }
+    }
+
+
+    [TestFixture]
+    public class OrderFactoryTests
+    {
+        public class OnLineServicesContext : DbContext
+        {
+            public DbSet<Order> Orders { get; set; }
+            public DbSet<Customer> Customers { get; set; }
+
+
+            public OnLineServicesContext() : base("OnLineServices")
+            {
+                
+            }
+        }
+
+        [Test]
+        public void OrderFactory_Instance()
+        {
+            var orderfactory = new OrderFactory();
+
+            var order = orderfactory.Create(new OrderInputData() {Created = DateTime.Today});
+
+            Assert.IsNotNull(order);
+
+            Assert.IsTrue(order.Created.Date.Equals(DateTime.Today));
+        }
+
+        [Test]
+        public void OrderFactory_Receives_OrderInputData()
+        {
+            OrderInputData input = new OrderInputData() {Created = DateTime.Today};
+            var orderFactory = new OrderFactory();
+            var repo = new OrderRepository();
+            var svc  =new BaseService<Order, OrderFactory, OrderRepository,OrderInputData>(orderFactory, repo);
+            svc.Create(input);
+            var custFactory   = new CustomerFactory();
+            var custRepo = new CustomerRepository();
+            CustomerInputData customerInput = new CustomerInputData() {FirstName = "Richard"};
+
+            var customerService = new BaseService<Customer, 
+                                                    CustomerFactory, 
+                                                    CustomerRepository, 
+                                                    CustomerInputData>(custFactory, custRepo);
+
+            customerService.Create(customerInput);
+        }
+
+
+        public class BaseService<TObject, TBaseFactory, TBaseRespository, TInputData>
+            where TObject : new()
+            where TBaseFactory : IFactory<TObject, TInputData>
+            where TBaseRespository : IRepository<TObject>
+        {
+            private TBaseFactory _factory;
+            private TBaseRespository _repository;
+
+            public BaseService(TBaseFactory factory, TBaseRespository repository)
+            {
+                _factory = factory;
+                _repository = repository;
+            }
+
+            public void Create(TInputData inputData)
+            {
+                var instance = _factory.Create(inputData);
+                if (instance != null)
+                {
+                   _repository.Add(instance);
+                }
+            }
+        }
+
+
+
+        public class OrderService
+        {
+            private BaseFactory<Order, OrderInputData> _factory;
+            private BaseRepository<Order> _repository;
+
+
+            public OrderService(BaseFactory<Order, OrderInputData> factory, BaseRepository<Order> repository )
+            {
+                _factory = factory;
+                _repository = repository;
+            }
+
+
+            public void Create(OrderInputData inputData)
+            {
+                var order = _factory.Create(inputData);
+                if (order != null)
+                {
+                    _repository.Add(order);
+                }
+            }
+        }
+
+        [Test]
+        public void FactoryWithInterface()
+        {
+            
+        }
+}
+   
+}
