eeb375940b795a8b82f345bb847dfc8b2108e318
Richard Jones <r_jones@constructix.com.au>
2016-12-15 14:48:51 +1000

    Added Bridge Design Pattern

---
+++ BridgeDesignPattern/BridgeDesignPattern/IBaseImplementor.cs
@@ -0,0 +1,17 @@
+﻿namespace BridgeDesignPattern
+{
+    public interface IBaseImplementor
+    {
+        void OperationImp();
+    }
+
+
+
+    public abstract class BaseLogProvider
+    {
+        public abstract void WriteLog(string message);
+    }
+
+
+
+}
