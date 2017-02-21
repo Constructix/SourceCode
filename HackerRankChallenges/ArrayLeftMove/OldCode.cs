ea852d00c602d27db3fcfa30ea1a359ec84acff2
Richard Jones <r_jones@constructix.com.au>
2017-02-22 08:30:06 +1000

    Updated Code

---
+++ HackerRankChallenges/ArrayLeftMove/OldCode.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Text;
+
+namespace HackerRank
+{
+    class OldCode
+    {
+        private static void TestMain()
+        {
+            int[] numbers = new int[GenerateArraySize()];
+            PopulateArray(numbers);
+            WriteArrayToFile(@"D:\Files\ArrayRotationData.txt", numbers);
+
+            int[] rotatedNumbers = new int[numbers.Length];
+            int currentIndex = 0;
+            int rotations = 2;
+
+
+            StringBuilder testBuilder = new StringBuilder();
+            for (int index = currentIndex; index < numbers.Length; index++)
+            {
+
+                int newIndex = NewPosition(index, rotations, numbers);
+                rotatedNumbers[newIndex] = numbers[index];
+            }
+            for (int index = 0; index < numbers.Length; index++)
+            {
+                testBuilder.AppendLine($"{numbers[index]} {rotatedNumbers[index]}");
+            }
+        }
+        private static int NewPosition(int currentIndex, int rotations, int[] numbers)
+        {
+            return (currentIndex + rotations) % numbers.Length;
+        }
+        private static void WriteArrayToFile(string FileName, int[] numbers)
+        {
+            StringBuilder builder = new StringBuilder();
+
+            for(int i=0; i < numbers.Length; i++)
+            {
+                builder.Append($"{numbers[i]} ");
+            }
+
+            builder = builder.Remove(builder.Length - 1, 1);
+
+            System.IO.File.WriteAllText(FileName, builder.ToString());
+        }
+
+        private static void PopulateArray(int[] numbers)
+        {
+            Random r = new Random();
+
+            for (int index = 0; index < numbers.Length; index++)
+            {
+                numbers[index] = r.Next(100000);
+            }
+        }
+
+        private static int GenerateArraySize()
+        {
+            Random r = new Random();
+            return r.Next(100000);
+        }
+    }
+}
--- HackerRankChallenges/ArrayLeftMove/Program.cs
+++ HackerRankChallenges/ArrayLeftMove/Program.cs
@@ -1,89 +1,61 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace HackerRank
+class Solution
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+
+        if (args.Length > 0)
         {
-
-            int[] numbers = new int[GenerateArraySize()];
-            PopulateArray(numbers);
-            WriteArrayToFile(@"D:\Files\ArrayRotationData.txt", numbers);
-
-            int[] rotatedNumbers = new int[numbers.Length];
-            int currentIndex = 0;
-            int rotations = 2;
-
-            
-
-            Stopwatch watch = new Stopwatch();
-            watch.Start();
-
-
-            StringBuilder testBuilder = new StringBuilder();
-
-
-            for (int index = currentIndex; index < numbers.Length; index++)
-            {
-
-                int newIndex = NewPosition(index, rotations, numbers);
-                rotatedNumbers[newIndex] = numbers[index];
-            }
-            watch.Stop();
-            
-            Console.WriteLine("Sample Output");
-            for(int index = 0; index < numbers.Length; index++)
-
-            {
-                testBuilder.AppendLine($"{numbers[index]} {rotatedNumbers[index]}");
-            }
-
-            System.IO.File.WriteAllText(@"D:\Files\Testing.dat", testBuilder.ToString());
-            System.Diagnostics.Process.Start(@"D:\Files\Testing.dat");
-
-            Console.WriteLine($"Number in Array: {numbers.Length}");
-            Console.WriteLine($"Total Time: { watch.Elapsed.ToString()}");
+            var reader = System.IO.File.OpenText(args[0]);
+            Console.SetIn(reader);
         }
 
-        private static void WriteArrayToFile(string FileName, int[] numbers)
+        string[] tokens_n = Console.ReadLine().Split(' ');
+        int arraySize = Convert.ToInt32(tokens_n[0]);
+        int rotations = Convert.ToInt32(tokens_n[1]);
+        int queries = Convert.ToInt32(tokens_n[2]);
+        string[] a_temp = Console.ReadLine().Split(' ');
+        int[] a = Array.ConvertAll(a_temp, Int32.Parse);
+
+        int[] queryIndex = new int[queries];
+        GetQueries(queries, queryIndex);
+        int[] rotatedNumbers = new int[arraySize];
+        int currentIndex = 0;
+
+        RotateArray(currentIndex, a, rotations, rotatedNumbers);
+        WriteRotatedArrayToConsole(queryIndex, rotatedNumbers);
+    }
+    
+    private static void WriteRotatedArrayToConsole(int [] queryIndex, int[] rotatedNumbers)
+    {
+        foreach (int index in queryIndex)
         {
-            StringBuilder builder = new StringBuilder();
-
-            for(int i=0; i < numbers.Length; i++)
-            {
-                builder.Append($"{numbers[i]} ");
-            }
-
-            builder = builder.Remove(builder.Length - 1, 1);
-
-            System.IO.File.WriteAllText(FileName, builder.ToString());
-        }
-
-        private static void PopulateArray(int[] numbers)
-        {
-            Random r = new Random();
-
-            for (int index = 0; index < numbers.Length; index++)
-            {
-                numbers[index] = r.Next(100000);
-            }
-        }
-
-        private static int NewPosition(int currentIndex, int rotations, int[] numbers)
-        {
-            return (currentIndex + rotations) % numbers.Length;
-        }
-
-        static int GenerateArraySize()
-        {
-            Random r = new Random();
-            return r.Next(100000);
+            Console.WriteLine(rotatedNumbers[index]);
         }
     }
+
+    private static void RotateArray(int currentIndex, int[] a, int rotations, int[] rotatedNumbers)
+    {
+        for (int index = currentIndex; index < a.Length; index++)
+        {
+            int newIndex = NewPosition(index, rotations, a);
+            rotatedNumbers[newIndex] = a[index];
+        }
+    }
+
+    private static void GetQueries(int queries, int[] queryIndex)
+    {
+        for (int a0 = 0; a0 < queries; a0++)
+        {
+            int m = Convert.ToInt32(Console.ReadLine());
+            queryIndex[a0] = m;
+        }
+    }
+
+    private static int NewPosition(int currentIndex, int rotations, int[] numbers)
+    {
+        return (currentIndex + rotations) % numbers.Length;
+    }
 }
+
