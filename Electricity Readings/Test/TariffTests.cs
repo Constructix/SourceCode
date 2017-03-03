4b79ee0c4ede21e86dd67d1d2d868437643b3a4e
Richard Jones <r_jones@constructix.com.au>
2017-03-03 21:36:27 +1000

    Seperated ElectricityCalculatorCalc into seperate code.

--- Electricity Readings/Test/TariffTests.cs
+++ Electricity Readings/Test/TariffTests.cs
@@ -5,30 +5,6 @@ using System.Linq;
 
 namespace Test
 {
-
-    public class ElectricityCalculatorTests
-    {
-        [Fact]
-        public void ElectricityCalculatorCalc()
-        {
-            var tariffs = ElectricityTestHelper.LoadTariffCollection();
-
-            var firstReading = new Reading {Value = 1000, RecordedOn = DateTime.Parse("01/03/2017")};
-            var secondReading = new Reading {Value = 2000, RecordedOn = DateTime.Now};
-
-            var usage = Reading.CalculateUsage(firstReading, secondReading);
-
-            var totalUsage =   ElectricityCalculator.Calculate(usage, ElectricityTestHelper.GetCurrent().Rate);
-
-
-            Assert.Equal(totalUsage, 12.50m);
-
-            Console.WriteLine(totalUsage);
-
-        }
-    }
-
-
     public class TariffTests
     {
         [Fact]
