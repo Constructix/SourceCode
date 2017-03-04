b6b55d957ff961c99c1c60cc914935158a772521
Richard Jones <r_jones@constructix.com.au>
2017-03-05 09:06:10 +1000

    Added myCodeCamp.Data project to the solution.

---
+++ Pluralsight/ImplementAndSecuringWebAPI/MyCodeCamp/src/MyCodeCamp/Controllers/CampsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyCodeCamp.Controllers
+{
+
+
+    //public class Order
+    //{
+    //    public Guid Id { get; set; }
+    //    public DateTime Created { get; set; }
+
+    //    public Order()
+    //    {
+    //        Id = Guid.NewGuid();
+    //        Created = DateTime.Now;
+
+    //    }
+    //}
+
+    [Route("api/[controller]")]
+    public class CampsController : Controller
+    {
+        // action
+        [HttpGet("")]
+        public IActionResult Get()
+        {
+            return Ok( new {Name = "Shawn", FavouriteColor = "Blue"});
+        }
+    }
+}
