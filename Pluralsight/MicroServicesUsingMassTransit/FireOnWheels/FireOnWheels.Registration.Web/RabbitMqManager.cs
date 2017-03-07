ccdcd878f89c71e03215c904e11993cd15e1fbb3
Richard Jones <r_jones@constructix.com.au>
2017-03-07 20:51:58 +1000

    Added more code.

---
+++ Pluralsight/MicroServicesUsingMassTransit/FireOnWheels/FireOnWheels.Registration.Web/RabbitMqManager.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Text;
+using FireOnWheels.Messaging;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+
+namespace FireOnWheels.Registration.Web
+{
+    public class RabbitMqManager : IDisposable
+    {
+        private readonly IModel channel;
+
+        public RabbitMqManager()
+        {
+            var connectionFactory = new ConnectionFactory {Uri = RabbitMqConstants.RabbitMqUri};
+            var connection = connectionFactory.CreateConnection();
+            channel = connection.CreateModel();
+            connection.AutoClose = true;
+        }
+
+        public void SendRegisterOrderCommand(IRegisterOrderCommand command)
+        {
+            channel.ExchangeDeclare(exchange:RabbitMqConstants.RegisterOrderExchange, type:ExchangeType.Direct);
+            channel.QueueDeclare(queue: RabbitMqConstants.RegisterOrderQueue, durable: false, exclusive: false, autoDelete: false, arguments: null);
+            channel.QueueBind(queue: RabbitMqConstants.RegisterOrderQueue,exchange:RabbitMqConstants.RegisterOrderExchange, routingKey:"");
+
+
+            var serialisedCommand = JsonConvert.SerializeObject(command);
+
+            var messageProperites = channel.CreateBasicProperties();
+            messageProperites.ContentType = RabbitMqConstants.JsonMimeType;
+            channel.BasicPublish(exchange: RabbitMqConstants.RegisterOrderExchange, 
+                                    routingKey:"", 
+                                    basicProperties: messageProperites, 
+                                    body: Encoding.ASCII.GetBytes(serialisedCommand));
+
+
+        }
+
+
+
+
+
+        public void Dispose()
+        {
+            if(channel.IsClosed)
+                channel?.Dispose();
+        }
+    }
+
+   
+}
---
+++ Pluralsight/MicroServicesUsingMassTransit/RabbitMqConstants.cs
@@ -0,0 +1,16 @@
+﻿namespace FireOnWheels.Messaging
+{
+    public class RabbitMqConstants
+    {
+        public const string RabbitMqUri = "amqp://guest:guest@localhost:15672/";
+        public const string JsonMimeType = "application/json";
+
+        public const string RegisterOrderExchange = "fireonwheels.registerorder.Exchange";
+        public const string RegisterOrderQueue = "fireonwheels.registerorder.queue";
+
+        public const string OrderRegisteredExchange = "fireonwheels.orderregistered.exchange";
+        public const string OrderRegisteredNotificationQueue  = "fireonwheels.orderregistered.notification.queue";
+
+
+    }
+}
