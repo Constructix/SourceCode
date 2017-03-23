9ec1141c35c47bf6a30fcf31970f1fd2f82c8beb
Richard Jones <r_jones@constructix.com.au>
2017-03-23 19:41:48 +1000

    HackerRank

---
+++ HackerRank/GridChallenge/GridChallenge/GridChallenge/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GridChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            if (args.Length == 1)
+            {
+                Console.SetIn(File.OpenText(args[0]));
+            }
+
+            int gridSize;
+
+            if (int.TryParse(Console.ReadLine(), out gridSize))
+            {
+                Console.WriteLine(gridSize);
+            }
+        }
+    }
+}
