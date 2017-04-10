4350ca56f06c963124f7154d81c23e2d6583e707
Richard Jones <r_jones@constructix.com.au>
2017-04-10 19:28:14 +1000

    checkin GridSearch

--- HackerRank/GridSearch/Program.cs
+++ HackerRank/GridSearch/Program.cs
@@ -22,36 +22,78 @@ class Solution
                 Console.SetIn(File.OpenText(args[0]));
             }
 
-            int tests = Convert.ToInt32(Console.ReadLine());
-            for (int a0 = 0; a0 < tests; a0++)
-            {
-                StringBuilder currentText = new StringBuilder();
 
-                string[] tokens_R = Console.ReadLine().Split(' ');
-                int totalRows = Convert.ToInt32(tokens_R[0]);
-                int TotalColumns = Convert.ToInt32(tokens_R[1]);
-                string[] dataGrid = new string[totalRows];
-                for (int currentGridRow = 0; currentGridRow < totalRows; currentGridRow++)
-                {
-                    dataGrid[currentGridRow] = Console.ReadLine();
-                }
-                string[] tokens_r = Console.ReadLine().Split(' ');
-                int r = Convert.ToInt32(tokens_r[0]);
-                int c = Convert.ToInt32(tokens_r[1]);
-                string[] P = new string[r];
-                bool prevMatch = true;
-                int startAtPosition = -1;
-                bool success = true;
+            Console.ReadLine();
+            var input = GetGridDimensions();
 
-                int currentRow, previousRow;
+            int[] gridDimensions = Array.ConvertAll(input, Convert.ToInt32);
 
-                int previousPosition = -1;
-                currentRow = previousRow = 0;
-                string newText = currentText.ToString();
-                CheckValues(r, P, newText,  prevMatch, previousPosition, TotalColumns, success);
-            }
-            Environment.Exit(Success);
+
+            int TotalRows = gridDimensions[0];
+            int TotalColumns = gridDimensions[1];
+
+
+            Console.WriteLine(TotalRows);
+            Console.WriteLine(TotalColumns);
+            
+            string[] Rows = new string[TotalRows];
+
+            LoadGridWithData(TotalRows, Rows);
+            int []searchGridDimensions = Array.ConvertAll(GetGridDimensions(), Convert.ToInt32);
+
+            Console.WriteLine($"{searchGridDimensions[0]}");
+            Console.WriteLine($"{searchGridDimensions[1]}");
+
+            string[] searchGrid = new string[searchGridDimensions[0]];
+
+            LoadGridWithData(searchGridDimensions[0], searchGrid);
+
+        //int tests = Convert.ToInt32(Console.ReadLine());
+        //for (int a0 = 0; a0 < tests; a0++)
+        //{
+        //    StringBuilder currentText = new StringBuilder();
+
+        //    string[] tokens_R = Console.ReadLine().Split(' ');
+        //    int totalRows = Convert.ToInt32(tokens_R[0]);
+        //    int TotalColumns = Convert.ToInt32(tokens_R[1]);
+        //    string[] dataGrid = new string[totalRows];
+        //    for (int currentGridRow = 0; currentGridRow < totalRows; currentGridRow++)
+        //    {
+        //        dataGrid[currentGridRow] = Console.ReadLine();
+        //    }
+        //    string[] tokens_r = Console.ReadLine().Split(' ');
+        //    int r = Convert.ToInt32(tokens_r[0]);
+        //    int c = Convert.ToInt32(tokens_r[1]);
+        //    string[] P = new string[r];
+        //    bool prevMatch = true;
+        //    int startAtPosition = -1;
+        //    bool success = true;
+
+        //    int currentRow, previousRow;
+
+        //    int previousPosition = -1;
+        //    currentRow = previousRow = 0;
+        //    string newText = currentText.ToString();
+        //    CheckValues(r, P, newText,  prevMatch, previousPosition, TotalColumns, success);
+        //}
+        Environment.Exit(Success);
         }
+
+    private static void LoadGridWithData(int TotalRows, string[] Rows)
+    {
+        for (int currentRowIndex = 0; currentRowIndex < TotalRows; currentRowIndex++)
+        {
+            Rows[currentRowIndex] = Console.ReadLine();
+            Console.WriteLine(Rows[currentRowIndex]);
+        }
+    }
+
+    private static string[] GetGridDimensions()
+    {
+        string[] input = Console.ReadLine().Split(new char[] {' '});
+        return input;
+    }
+
     private static void CheckValues(int r, string[] searchPattern, string newText, bool prevMatch,
         int previousPosition, int columnCount, bool success)
     {
---
+++ HackerRank/GridSearchTests/Grid.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace GridSearchTests
+{
+    public class Grid
+    {
+
+        private int[,] _internalGrid;
+        private int _rowsAllocated = 0;
+
+        public Grid()
+        {
+        }
+
+        public Grid(int rows, int columns)
+        {
+            _internalGrid = new int[rows, columns];
+        }
+
+        public int this[int index, int columnIndex]
+        {
+            get { return _internalGrid[index, columnIndex]; }
+            set { _internalGrid[index, columnIndex] = value; }
+
+        }
+
+        public void AddRow(string testInputValue)
+        {
+            int newIntValue = 0;
+
+
+            Validate(testInputValue);
+
+            for (int index = 0; index < _internalGrid.GetLength(1); index++) { 
+                if (int.TryParse(testInputValue.Substring(index, 1), out newIntValue))
+                {
+                    _internalGrid[_rowsAllocated, index] = newIntValue;
+                }
+            }
+
+            ++_rowsAllocated;
+        }
+
+        private void Validate(string testInputValue)
+        {
+            if (_rowsAllocated >= _internalGrid.GetLength(0))
+                throw new Exception("Tryin to add a row that is larger than Row length.");
+
+            if (testInputValue.Length > _internalGrid.GetLength(1))
+                throw new Exception("Trying to add column that is larger than the column length.");
+        }
+    }
+}
