c82cdd791539d119a5df45d9508fe8d25107cee8
Richard Jones <r_jones@constructix.com.au>
2017-04-10 20:47:11 +1000

    Added SearchTable Test

--- HackerRank/GridSearchTests/GridTests.cs
+++ HackerRank/GridSearchTests/GridTests.cs
@@ -62,7 +62,8 @@ namespace GridSearchTests
         {
             var testInputValue = "7283455864";
             var testSecondColumnValue = "6731158619";
-            var expectedValues = new int[2, 10] {{7, 2, 8, 3, 4, 5, 5, 8, 6, 4}, {6, 7, 3, 1, 1, 5, 8, 6, 1, 9}};
+            var expectedValues = new int[2, 10] {{7, 2, 8, 3, 4, 5, 5, 8, 6, 4}, 
+                                                 {6, 7, 3, 1, 1, 5, 8, 6, 1, 9}};
 
 
             var DataGrid = new Grid(2, 10);
@@ -81,7 +82,25 @@ namespace GridSearchTests
         [Fact]
         public void DataTable_SearchTable()
         {
-            
+            var firstRowInputValue = "9505";
+            var secondRowInputValue = "3845";
+            var thirRow = "3530";
+
+            var expectedValues = new int[3, 4] {{9, 5, 0, 5}, {3, 8, 4, 5}, {3, 5, 3, 0}};
+
+            var searchPatternGrid = new Grid(3,4);
+
+            searchPatternGrid.AddRow(firstRowInputValue);
+            searchPatternGrid.AddRow(secondRowInputValue);
+            searchPatternGrid.AddRow(thirRow);
+
+            for (int rowIndex = 0; rowIndex < 3; rowIndex++)
+            {
+                for (int columnIndex = 0; columnIndex < 4; columnIndex++)
+                {
+                    Assert.True(searchPatternGrid[rowIndex, columnIndex] == expectedValues[rowIndex, columnIndex]);
+                }
+            }
         }
 
 
