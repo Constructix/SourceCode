da41d5a48ce0982c67c732061d5aa5792b5c4e5e
Richard Jones <r_jones@constructix.com.au>
2017-07-13 14:42:11 +1000

    Updated the Repository

---
+++ Constructix-Repository/Constructix.Business.Data.Database/Database.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+using Constructix.Business.Data.Entities;
+
+namespace Constructix.Business.Data.Database
+{
+    public class Database : DbContext
+    {
+
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+
+        public DbSet<Supplier> Suppliers { get; set; }
+        
+        
+
+        public Database() : base("DataServices")
+        {
+            
+        }
+
+        public Database(string dataSourceName) : base(dataSourceName)
+        {
+        }
+
+       
+    }
+}
---
+++ Constructix-Repository/Constructix.Business.Data.Database/Migrations/Configuration.cs
@@ -0,0 +1,31 @@
+namespace Constructix.Business.Data.Database.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Constructix.Business.Data.Database.Database>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(Constructix.Business.Data.Database.Database context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
