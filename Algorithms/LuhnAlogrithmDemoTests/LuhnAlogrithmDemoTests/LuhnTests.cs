ba01bb69d99dc5b5713599ea9350553209fb5a14
Richard Jones <r_jones@constructix.com.au>
2017-10-13 18:12:45 +1000

    Updated Unit test and renamed parameter to be expectedResult

--- Algorithms/LuhnAlogrithmDemoTests/LuhnAlogrithmDemoTests/LuhnTests.cs
+++ Algorithms/LuhnAlogrithmDemoTests/LuhnAlogrithmDemoTests/LuhnTests.cs
@@ -9,14 +9,14 @@ namespace LuhnAlogrithmDemo
         }
 
         [Theory, 
-         InlineData("79927398713", 3, true),
-         InlineData("79927398710", 0, false),
-         InlineData("79927398711", 1, false)
+         InlineData("79927398713",  true),
+         InlineData("79927398710",  false),
+         InlineData("79927398711",  false)
         ]
-        public void LuhnAlgorithm(string inputNumber, int expectedCheckDigit, bool expectedIsValid)
+        public void LuhnAlgorithm(string inputNumber, bool expectedResult)
         {
             var result = Luhn.Check(inputNumber);
-            Assert.True(expectedIsValid == result);
+            Assert.True(expectedResult == result);
         }
     }
 }
