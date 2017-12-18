ca05e10d8a6547fb161293647c65a05f8c62e3d4
Richard Jones <r_jones@constructix.com.au>
2017-12-18 15:30:53 +1000

    seperated billing service into class.

--- Constructix.Home.ElectricityManagement/Constructix.Home.ElectricityReadingManagement.Tests/Constructix.Home.ElectricityReadingManagement.Tests/BillingServiceTests.cs
+++ Constructix.Home.ElectricityManagement/Constructix.Home.ElectricityReadingManagement.Tests/Constructix.Home.ElectricityReadingManagement.Tests/BillingServiceTests.cs
@@ -1,37 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using Constructix.Home.Electricity.Business.DomainModels.Readings;
+using Constructix.Home.Electricity.Business.DomainModels.Services;
 using Constructix.Home.Electricity.Business.DomainModels.Tariffs.Implementors;
-using Constructix.Home.ElectricityReadingManagement.Old_Tests;
 using Xunit;
-using TariffService = Constructix.Home.Electricity.Business.DomainModels.Services.TariffService;
 
 namespace Constructix.Home.ElectricityReadingManagement
 {
-    public class TariffServiceTests
+    public class BillingServiceTests
     {
-        [Fact]
-        public void TariffServiceReturnsTariffRates()
+        private readonly List<BaseTariff> _tariffs = new List<BaseTariff>();
+
+        public BillingServiceTests()
         {
-            List<BaseTariff> baseTariffs = new List<BaseTariff>
+            _tariffs = new List<BaseTariff>
             {
                 new ElectricityTariff(0.2461m, DateTime.Parse("01/07/2016"), DateTime.Parse("30/06/2017")),
                 new ElectricityTariff(0.26m, DateTime.Parse("01/07/2017"), null),
                 new  HotWaterTarff(0.1696m, DateTime.Parse("01/07/2016"), DateTime.Parse("30/06/2017")),
                 new  HotWaterTarff(0.1696m, DateTime.Parse("01/07/2016"), null),
-
             };
-
-            Reading previousReading =  new Reading(1000, DateTime.Parse("01/10/2016"), TariffType.Electricity);
+        }
+        [Fact]
+        public void BillingService_CreateElectricityTotal()
+        {
+            Reading previousReading = new Reading(1000, DateTime.Parse("01/10/2016"), TariffType.Electricity);
             Reading latestReading = new Reading(1200, DateTime.Parse("01/03/2017"), TariffType.Electricity);
 
-            decimal expectedRate = 0.2461m;
 
 
-            var tariffSvc = new TariffService(baseTariffs);
-            var firstTariff =  tariffSvc.GetTariffs(previousReading, latestReading).Select(x => x.Rate);
-            Assert.True(firstTariff.FirstOrDefault().Equals(expectedRate));
+
+            var tariffSvc  =new TariffService(_tariffs);
+
+            BillingService billingSvc = new BillingService(tariffSvc);
+
+
+
+
         }
     }
 }
