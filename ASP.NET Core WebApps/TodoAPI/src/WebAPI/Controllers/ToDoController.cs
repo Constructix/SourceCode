2f2aac9a0a0d9e13ed2d817d7983e91110e85e08
Richard Jones <r_jones@constructix.com.au>
2018-10-14 16:38:36 +1000

    Revert "Updated structure of solution. Cleaner"
    This reverts commit db86b0490d9a190b1d9e81ff1265c751d145835c.

---
+++ ASP.NET Core WebApps/TodoAPI/src/WebAPI/Controllers/ToDoController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using TodoAPI.Models;
+
+namespace TodoAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ToDoController : ControllerBase
+    {
+
+        private readonly TodoContext _context;
+
+        public ToDoController(TodoContext context)
+        {
+            _context = context;
+
+            if (_context.TodoItems.Count() == 0)
+            {
+                _context.TodoItems.Add(new TodoItem {Name = "Item1"});
+                _context.SaveChanges();
+            }
+        }
+
+        [HttpGet]
+        public ActionResult<List<TodoItem>> GetAll()
+        {
+            return _context.TodoItems.ToList();
+        }
+
+        [HttpGet("{id}", Name = "GetTodo")]
+        public ActionResult<TodoItem> GetById(int id)
+        {
+            var item = _context.TodoItems.Find(id);
+            if(item == null)
+            {
+                return NotFound();
+            }
+
+            return item;
+        }
+    }
+}
