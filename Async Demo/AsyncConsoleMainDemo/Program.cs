ec5e3e8ae0a6498b50fde7e1ba30e346fe6f0850
Richard Jones <r_jones@constructix.com.au>
2018-12-17 12:57:54 +1000

    Added Asyn console Main

---
+++ Async Demo/AsyncConsoleMainDemo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net.Http;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace AsyncConsoleMainDemo
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            //GetWebSites().GetAwaiter().GetResult();
+            await GetWebSites();
+        }
+
+
+        private static async Task GetWebSites()
+        {
+            var webSites = new string[]
+            {
+                "http://www.constructix.com.au", 
+                "http://www.bing.com", 
+                "http://oracle.com"
+            };
+
+
+            foreach (string webSite in webSites)
+            {
+                var result = await GetResults(webSite);
+                Console.WriteLine(result.Length);
+            }
+
+        }
+
+        public static async Task<string> GetResults(string webAddress)
+        {
+            var result = await new HttpClient().GetStringAsync(webAddress);
+
+            return result;
+        }
+    }
+}
