2f2b2d12fa46c4dddcaa2037d5a1dbc9eb4d9657
Richard Jones <r_jones@constructix.com.au>
2018-12-17 12:24:12 +1000

    Added AWS and Rekognition

---
+++ AWS/AmazonRekognitionDemo/AmazonRekognitionDemo1/Program.cs
@@ -0,0 +1,83 @@
+﻿/*******************************************************************************
+* Copyright 2009-2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+* 
+* Licensed under the Apache License, Version 2.0 (the "License"). You may
+* not use this file except in compliance with the License. A copy of the
+* License is located at
+* 
+* http://aws.amazon.com/apache2.0/
+* 
+* or in the "license" file accompanying this file. This file is
+* distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+* KIND, either express or implied. See the License for the specific
+* language governing permissions and limitations under the License.
+*******************************************************************************/
+
+using System;
+using System.Collections.Specialized;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Net;
+
+using Amazon;
+using Amazon.Rekognition;
+using Amazon.Rekognition.Model;
+
+namespace AmazonRekognitionDemo1
+{
+    class AmazoneReckognition
+    {
+        // Change the AWSProfileName to the profile you want to use in the App.config file.
+        // See http://aws.amazon.com/credentials  for more details.
+        // You must also sign up for an Amazon S3 account for this to work
+        // See http://aws.amazon.com/s3/ for details on creating an Amazon S3 account
+        // Change the bucketName and keyName fields to values that match your bucketname and keyname
+        
+
+        public static void Main(string[] args)
+        {
+            string photoLocation =  @"D:\Photos\image.adapt.1920.medium.jpg";
+            Amazon.Rekognition.Model.Image image = new Amazon.Rekognition.Model.Image();
+        try
+        {
+            using (FileStream fs = new FileStream(photoLocation, FileMode.Open, FileAccess.Read))
+            {
+                byte[] data = null;
+                data = new byte[fs.Length];
+                fs.Read(data, 0, (int)fs.Length);
+                image.Bytes = new MemoryStream(data);
+            }
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Failed to load file " + photoLocation);
+            return;
+        }
+
+        AmazonRekognitionClient rekognitionClient = new AmazonRekognitionClient();
+
+        DetectLabelsRequest detectlabelsRequest = new DetectLabelsRequest()
+        {
+            Image = image,
+            MaxLabels = 10,
+            MinConfidence = 77F
+        };
+
+        try
+        {
+            DetectLabelsResponse detectLabelsResponse = rekognitionClient.DetectLabels(detectlabelsRequest);
+            Console.WriteLine("Detected labels for " + photoLocation);
+            foreach (Label label in detectLabelsResponse.Labels)
+                Console.WriteLine("{0}: {1}", label.Name, label.Confidence);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+
+
+        }
+    }
+
+}
---
+++ AWS/AmazonRekognitionDemo/CompareFacesRekognitionDemo/Program.cs
@@ -0,0 +1,105 @@
+﻿/*******************************************************************************
+* Copyright 2009-2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+* 
+* Licensed under the Apache License, Version 2.0 (the "License"). You may
+* not use this file except in compliance with the License. A copy of the
+* License is located at
+* 
+* http://aws.amazon.com/apache2.0/
+* 
+* or in the "license" file accompanying this file. This file is
+* distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+* KIND, either express or implied. See the License for the specific
+* language governing permissions and limitations under the License.
+*******************************************************************************/
+
+using System;
+using System.Collections.Specialized;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Net;
+
+using Amazon;
+using Amazon.Rekognition;
+using Amazon.Rekognition.Model;
+
+namespace CompareFacesRekognitionDemo
+{
+    class S3Sample
+    {
+        // Change the AWSProfileName to the profile you want to use in the App.config file.
+        // See http://aws.amazon.com/credentials  for more details.
+        // You must also sign up for an Amazon S3 account for this to work
+        // See http://aws.amazon.com/s3/ for details on creating an Amazon S3 account
+        // Change the bucketName and keyName fields to values that match your bucketname and keyname
+        public static void Main(string[] args)
+        {
+
+            float similarityThreshold = 70F;
+            string sourceImage =  String.Empty;
+            string targetImage = string.Empty;
+
+        AmazonRekognitionClient rekognitionClient = new AmazonRekognitionClient();
+
+        Amazon.Rekognition.Model.Image imageSource = new Amazon.Rekognition.Model.Image();
+        try
+        {
+            using (FileStream fs = new FileStream(sourceImage, FileMode.Open, FileAccess.Read))
+            {
+                byte[] data = new byte[fs.Length];
+                fs.Read(data, 0, (int)fs.Length);
+                imageSource.Bytes = new MemoryStream(data);
+            }
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Failed to load source image: " + sourceImage);
+            return;
+        }
+
+        Amazon.Rekognition.Model.Image imageTarget = new Amazon.Rekognition.Model.Image();
+        try
+        {
+            using (FileStream fs = new FileStream(targetImage, FileMode.Open, FileAccess.Read))
+            {
+                byte[] data = new byte[fs.Length];
+                data = new byte[fs.Length];
+                fs.Read(data, 0, (int)fs.Length);
+                imageTarget.Bytes = new MemoryStream(data);
+            }
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Failed to load target image: " + targetImage);
+            return;
+        }
+
+        CompareFacesRequest compareFacesRequest = new CompareFacesRequest()
+        {
+            SourceImage = imageSource,
+            TargetImage = imageTarget,
+            SimilarityThreshold = similarityThreshold
+        };
+
+        // Call operation
+        CompareFacesResponse compareFacesResponse = rekognitionClient.CompareFaces(compareFacesRequest);
+
+        // Display results
+        foreach(CompareFacesMatch match in compareFacesResponse.FaceMatches)
+        {
+            ComparedFace face = match.Face;
+            BoundingBox position = face.BoundingBox;
+            Console.WriteLine("Face at " + position.Left
+                  + " " + position.Top
+                  + " matches with " + face.Confidence
+                  + "% confidence.");                
+            Console.WriteLine($"Height :{position.Height} Width: {position.Width}");
+        }
+
+        Console.WriteLine("There was " + compareFacesResponse.UnmatchedFaces.Count + " face(s) that did not match");
+        Console.WriteLine("Source image rotation: " + compareFacesResponse.SourceImageOrientationCorrection);
+        Console.WriteLine("Target image rotation: " + compareFacesResponse.TargetImageOrientationCorrection);
+        }     
+    }
+}
