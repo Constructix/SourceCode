c35384fc384352470b909f245c2f00e7ca9de13e
Richard Jones <r_jones@constructix.com.au>
2019-05-16 21:26:44 +1000

    Add unit tests

--- ToDoWebAPI/ToDoWebAPI.Models/TodoItem.cs
+++ ToDoWebAPI/ToDoWebAPI.Models/TodoItem.cs
@@ -1,9 +1,18 @@
-﻿namespace ToDoAPI.Models
+﻿using System;
+
+namespace ToDoAPI.Models
 {
     public class TodoItem
     {
         public long Id { get; set; }
         public string Name { get; set; }
         public bool IsComplete { get; set; }
+
+        public DateTime Created { get; }
+
+        public TodoItem()
+        {
+            Created  = DateTime.Now.ToUniversalTime();
+        }
     }
 }
