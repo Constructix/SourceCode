6391c62719e5c5f0df5fe7ca4f34d6f1d1ef853d
Richard Jones <r_jones@constructix.com.au>
2019-06-13 20:34:39 +1000

    Initial Checkin into gitHub

---
+++ WebAPIMultipleInstances/SetEndpointAddressDemo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace SetEndpointAddressDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+           // CreateWebHostBuilder(args).Build().Run();
+           var urls = new List<string> {args[1]};
+
+
+           var configuration = new ConfigurationBuilder()
+               .AddCommandLine(args)
+               .Build();
+
+           var host = new WebHostBuilder()
+               .UseKestrel()
+               .UseContentRoot(Directory.GetCurrentDirectory())
+               .UseIISIntegration()
+               .UseConfiguration(configuration)
+               .UseStartup<Startup>()
+               .UseUrls( urls.ToArray())
+               .Build();
+
+            host.Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
---
+++ WebAPIMultipleInstances/StartupMultipleAPIs/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace StartupMultipleAPIs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //dotnet publish -c Release -r win-x64 --self-contained false
+
+            List<string> urls = new List<string> {"https://localhost:3333/", "https://localhost:3334/"};
+            ;
+            Console.WriteLine("About to start up Multiple Endpoints....");
+
+
+            Task.Run(() =>
+            {
+                foreach (var url in urls)
+                {
+                    ProcessStartInfo psi = new ProcessStartInfo(
+                        @"D:\Development\GitHub\WebAPIDemo\SetEndpointAddressDemo\bin\Release\netcoreapp2.2\win-x64\SetEndpointAddressDemo.exe",
+                        $"--n {url}");
+                    System.Diagnostics.Process.Start(psi);
+
+                }
+            });
+
+            //ProcessStartInfo psi = new ProcessStartInfo(@"D:\Development\GitHub\WebAPIDemo\SetEndpointAddressDemo\bin\Release\netcoreapp2.2\win-x64\SetEndpointAddressDemo.exe", "--n https://localhost:3333/");
+
+            //System.Diagnostics.Process.Start(psi);
+
+            Console.WriteLine("Hit Enter to end....");
+            Console.ReadLine();
+        }
+    }
+}
