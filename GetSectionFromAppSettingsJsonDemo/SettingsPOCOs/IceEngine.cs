3441a518715db6ae36a034039796d43a663c18fb
Richard Jones <r_jones@constructix.com.au>
2019-07-07 10:13:19 +1000

    Updated IceEngine

--- GetSectionFromAppSettingsJsonDemo/SettingsPOCOs/IceEngine.cs
+++ GetSectionFromAppSettingsJsonDemo/SettingsPOCOs/IceEngine.cs
@@ -3,6 +3,6 @@
     public class IceEngine
     {
         public string EndPointAddress { get; set; }
-        public Methods [] Methods { get; set; }
+        public WebMethod [] WebMethods { get; set; }
     }
 }
