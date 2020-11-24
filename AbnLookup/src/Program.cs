15f5b0aaf42b6e457658192be67cf0196d5be5eb
[r_jones@constructix.com.au] <r_jones@constructix.com.au>
2020-11-24 18:22:58 +1000

    Added HttpClientFactory and improved the error handling response.

--- AbnLookup/src/Program.cs
+++ AbnLookup/src/Program.cs
@@ -11,47 +11,17 @@ namespace AbnLookup
         {
             var config = SetupConfiguration();
 
+            var recordsToRetrieve = ValidateInputParameters(args, config);
 
-            if (args.Length != 2)
-            {
-                Console.WriteLine("Usage: AbnLookup [Name] [NumberOfRecords]");
-                Console.WriteLine();
-                Console.WriteLine("[name] - Name of company, entity to retrieve.");
-                Console.WriteLine("[NumberOfRecords] - Number of records to return, eg: 10 - returns up to 10 records.");
-                Console.WriteLine();
-                Environment.Exit(1);
-            }
-            else
-            {
-                if (!int.TryParse(args[1], out int  maxRecords))
-                {
-                    Console.WriteLine("Usage: AbnLookup [Name] [NumberOfRecords]");
-                    Console.WriteLine();
-                    Console.WriteLine("NumberOfRecords has been specified incorrectly.");
-                    Console.WriteLine("[NumberOfRecords] - Number of records to return,  eg: AbnLookup \"test Company\" \"10\"");
-                    Console.WriteLine();
-                    Environment.Exit(1);
-                }
-            }
-
-
-            if (string.IsNullOrWhiteSpace(config["ApiKey"]))
-            {
-                Console.WriteLine("API Key has not been set. Exiting program.");
-                Environment.Exit(1);
-            }
-
-           
-
-            HttpClient client = new HttpClient();
+            HttpClient client = HttpClientFactory.Create();
 
             var abnLookUp = new AbnLookup(client, config);
 
             var response = await abnLookUp.NameLookup(new NameLookupRequest
             {
                 NameOfEntity = args[0],
-                MaxResultsToReturn = int.Parse(args[1])
-                
+                MaxResultsToReturn = recordsToRetrieve
+
             });
 
             if (string.IsNullOrWhiteSpace(response.Message))
@@ -68,6 +38,44 @@ namespace AbnLookup
                 Console.WriteLine(response.Message);
             }
             Environment.Exit(0);
+        }
+
+        private static int  ValidateInputParameters(string[] args, IConfigurationRoot config)
+        {
+
+            int recordsToRetrieve = -1;
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Usage: AbnLookup [Name] [NumberOfRecords]");
+                Console.WriteLine();
+                Console.WriteLine("[name] - Name of company, entity to retrieve.");
+                Console.WriteLine("[NumberOfRecords] - Number of records to return, eg: 10 - returns up to 10 records.");
+                Console.WriteLine();
+                Environment.Exit(1);
+            }
+            else
+            {
+                if (!int.TryParse(args[1], out int maxRecords))
+                {
+                    Console.WriteLine("Usage: AbnLookup [Name] [NumberOfRecords]");
+                    Console.WriteLine();
+                    Console.WriteLine("NumberOfRecords has been specified incorrectly.");
+                    Console.WriteLine(
+                        "[NumberOfRecords] - Number of records to return,  eg: AbnLookup \"test Company\" \"10\"");
+                    Console.WriteLine();
+                    Environment.Exit(1);
+                }
+                else
+                    recordsToRetrieve = maxRecords;
+            }
+
+
+            if (string.IsNullOrWhiteSpace(config["ApiKey"]))
+            {
+                Console.WriteLine("API Key has not been set. Exiting program.");
+                Environment.Exit(1);
+            }
+            return recordsToRetrieve;
         }
 
         private static IConfigurationRoot SetupConfiguration()
