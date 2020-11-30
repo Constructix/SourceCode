1a77c97feaf0f877c82a574df12e8233b5b1adb0
[r_jones@constructix.com.au] <r_jones@constructix.com.au>
2020-11-30 14:05:19 +1000

    Adding Behaviours to the Duck

--- DesignPatterns/Net5/src/behaviour/BehaviourDesignPattern/Program.cs
+++ DesignPatterns/Net5/src/behaviour/BehaviourDesignPattern/Program.cs
@@ -6,11 +6,82 @@ namespace BehaviourDesignPattern
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(" ------  Behaviour Design Pattern Demostration  ---------");
+            Console.Clear();
+            Console.WriteLine(" ------  Behaviour Design Pattern Demonstration  ---------");
+
+            Duck duck = new Duck(new QuackBehaviour(), new FlyWithWings());
             
-            
-            
+            duck.PerformQuack();
+            duck.PerformFly();
+        }
+    }
+
+    public class Duck
+    {
+
+        public IQuackBehaviour _behaviour;
+        public IFlyBehaviour _flyBehaviour;
+
+        public Duck(IQuackBehaviour behaviour, IFlyBehaviour flyBehaviour)
+        {
+            _behaviour = behaviour;
+            _flyBehaviour = flyBehaviour;
+        }
+        public void PerformQuack() => _behaviour.Quack();
+
+        public void PerformFly() => _flyBehaviour.Fly();
+    }
+    
+    
+    // Behaviours
+    #region Quack
+    public interface IQuackBehaviour
+    {
+        public void Quack();
+    }
+    public class QuackBehaviour : IQuackBehaviour
+    {
+        public void Quack()
+        {
+            Console.WriteLine("QUACK.....");
+        }
+    }
+
+    public class SqueakBehaviour : IQuackBehaviour
+    {
+        public void Quack()
+        {
+            Console.WriteLine("SQUEAK.....");
             
         }
     }
+    public class MuteBehaviour : IQuackBehaviour
+    {
+        public void Quack()
+        {
+            Console.WriteLine("MUTE......");
+            
+        }
+    }
+    #endregion    
+    
+    #region Fly
+
+    public interface IFlyBehaviour
+    {
+        public void Fly();
+    }
+    
+    public class FlyWithWings : IFlyBehaviour
+    {
+        public void Fly() => Console.WriteLine("FLYING .......");
+       
+    }
+    
+    public class FlyNoWings :IFlyBehaviour
+    {
+        public void Fly() => Console.WriteLine("NOT FLYING......");
+    }
+
+    #endregion
 }
