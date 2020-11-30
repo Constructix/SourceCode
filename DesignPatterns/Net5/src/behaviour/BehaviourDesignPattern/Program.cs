65576d4ee7edd6e2203fd8ace91c43517598881d
[r_jones@constructix.com.au] <r_jones@constructix.com.au>
2020-11-30 13:46:00 +1000

    Adding DesignPatterns .net 5

---
+++ DesignPatterns/Net5/src/behaviour/BehaviourDesignPattern/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace BehaviourDesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" ------  Behaviour Design Pattern Demostration  ---------");
+            
+            
+            
+            
+        }
+    }
+}
