b37d62cd1ab6f36ffe415a9b1d12a806a4c6adcb
Richard Jones <rjjones79@hotmail.com>
2020-12-10 11:13:58 +1000

    Revert back

--- Azure/AzureFunctions/AZ_Func_ConstructixConvertToUpperCase/AZ_FUNC_ConstructixConvertToUpperCase/Functions/UpperCaseFunction.cs
+++ Azure/AzureFunctions/AZ_Func_ConstructixConvertToUpperCase/AZ_FUNC_ConstructixConvertToUpperCase/Functions/UpperCaseFunction.cs
@@ -1,13 +1,11 @@
 using System;
 using System.IO;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Primitives;
 using Newtonsoft.Json;
 
 namespace AZ_FUNC_ConstructixConvertToUpperCase.Functions
@@ -19,19 +17,11 @@ namespace AZ_FUNC_ConstructixConvertToUpperCase.Functions
             HttpRequest request,
             ILogger log)
         {
-
-            request.Headers.TryGetValue("X-Forwarded-For", out StringValues values);
-
-            if (values.ToArray().Any())
-            {
-                var value = values.First();
-            }
-
             log.LogInformation("UpperCaseFunction has been called.");
 
             string requestBody = await new StreamReader(request.Body).ReadToEndAsync();
             var upperCaseRequest = JsonConvert.DeserializeObject<UpperCaseRequest>(requestBody);
-            var upperCaseResponse = new UpperCaseResponse() {Message = upperCaseRequest.Message.ToUpper(), Created =  DateTimeOffset.Now, Values  =values.ToList()};
+            var upperCaseResponse = new UpperCaseResponse() {Message = upperCaseRequest.Message.ToUpper(), Created =  DateTimeOffset.Now};
 
             return new OkObjectResult(upperCaseResponse);
 
--- Azure/AzureFunctions/AZ_Func_ConstructixConvertToUpperCase/AZ_FUNC_ConstructixConvertToUpperCase/UpperCaseResponse.cs
+++ Azure/AzureFunctions/AZ_Func_ConstructixConvertToUpperCase/AZ_FUNC_ConstructixConvertToUpperCase/UpperCaseResponse.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections.Generic;
 
 namespace AZ_FUNC_ConstructixConvertToUpperCase
 {
@@ -7,6 +6,5 @@ namespace AZ_FUNC_ConstructixConvertToUpperCase
     {
         public string Message { get; set; }
         public DateTimeOffset Created { get; set; }
-        public List<string> Values { get; set; }
     }
 }
