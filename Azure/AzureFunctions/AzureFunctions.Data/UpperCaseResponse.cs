38dcd4275f504e259b10a987ea02e8d6b95fb23d
Richard Jones <rjjones79@hotmail.com>
2020-12-10 10:26:18 +1000

    Updating Azure Functions

--- Azure/AzureFunctions/AzureFunctions.Data/UpperCaseResponse.cs
+++ Azure/AzureFunctions/AzureFunctions.Data/UpperCaseResponse.cs
@@ -1,7 +1,7 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 
-namespace AZ_FUNC_ConstructixConvertToUpperCase
+namespace AzureFunctions.Data
 {
     public class UpperCaseResponse
     {
